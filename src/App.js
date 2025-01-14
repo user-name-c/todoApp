de7b30829215abbbc6d9f5b730f9d4fb8ae051a8
user-name-c <ivandgr_16@hotmail.com>
2025-01-13 19:27:35 -0600

    create elements in the web page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import{TodoCounter} from './TodoCounter';
+import{TodoSearch} from './TodoSearch';
+import{TodoList} from './TodoList';
+import{TodoItem} from './TodoItem';
 import logo from './platzi.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter/>
+      <TodoSearch/>
+    
+      <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+      </TodoList>
+    
+      {/* <CreateTodoButton/> */}
     </div>
+  );
+}
+
+function TodoCount(props){
+  return(
+    <h1>
+      Completaste
+      {props.completed} de 
+      {props.total} TODOs
+    </h1>
   );
 }
 
