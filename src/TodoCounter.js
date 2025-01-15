b51ded5d867e2ab7d06040a01dc0ebbd382972fc
user-name-c <ivandgr_16@hotmail.com>
2025-01-14 19:50:50 -0600

    Add props to the elements

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,7 @@
-function TodoCounter() {
+function TodoCounter({total, completed}) {
     return(
         <h1>
-            Has completado 3 de 5 TODOs
+            Has completado {completed} de {total} TODOs
         </h1>
     );
 }
