545bd36f8b35b389d5fc3ac761b09d94d23221b8
ivan <ivandgr_16@hotmail.com>
2025-02-20 18:10:43 -0600

    Update props for better component behavior

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
+// import './App.css';
 import React from 'react';
 
 const defaultTodos = [
