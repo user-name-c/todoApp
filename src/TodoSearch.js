922026e1966d2e221b0c76d50c5991c2a6b59131
ivan <ivandgr_16@hotmail.com>
2025-02-22 19:18:35 -0600

    Add event handlers to buttons

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -5,6 +5,13 @@ function TodoSearch() {
         <input 
         placeholder="Cortar cebolla"
         className='TodoSearch'
+        onChange={(event) =>{
+            console.log('Escribiste en el TodoSearch');
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);
+            }
+        }
         />
     );
 }
