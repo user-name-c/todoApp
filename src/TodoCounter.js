97a2aa1455aa3e520887ff8894c38df4212e5de9
ivan <ivandgr_16@hotmail.com>
2025-02-28 19:56:17 -0600

    Display a congratulatory message when all tasks are done

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,10 @@ import'./TodoCounter.css'
 function TodoCounter({total, completed}) {
     return(
         <h1 className="TodoCounter">
-            Has completado {completed} de {total} TODOs
+            { total === completed
+                ? 'Felicidades, completaste todos los TODOs!!'
+                : `Has completado ${completed} de ${total} TODOs`
+            }
         </h1>
     );
 }
