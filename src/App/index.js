591f9ae2c622b08dd96176e6bea6a776cc8d1e81
ivan <ivandgr_16@hotmail.com>
2025-03-12 18:15:53 -0600

    Refactor: organize components into separate folders

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,10 @@
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
-// import './App.css';
 import React from 'react';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+// import './App.css';
 
 // const defaultTodos = [
 //   { text: 'Cortar cebolla', completed: true },
