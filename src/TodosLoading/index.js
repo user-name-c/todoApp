e3052b1ba1707eb318027ee7e3abef6bc9acabc0
ivan <ivandgr_16@hotmail.com>
2025-03-19 20:59:34 -0600

    Add custom loading animation

--- src/TodosLoading/index.js
+++ src/TodosLoading/index.js
@@ -3,10 +3,15 @@ import './TodosLoading.css';
 
 function TodosLoading() {
     return(
-    <div className="LoadingTodo-container">
-        <span className="LoadingTodo-completeIcon"></span>
-        <p className="LoadingTodo-text"></p>
-        <span className="LoadingTodo-deleteIcon"></span>
+    // <div className="LoadingTodo-container">
+    //     <span className="LoadingTodo-completeIcon"></span>
+    //     <p className="LoadingTodo-text"></p>
+    //     <span className="LoadingTodo-deleteIcon"></span>
+    // </div>
+    <div className="loading-container">
+        <div className="loading-circle"></div>
+        <div className="loading-bar"></div>
+        <div className="loading-circle"></div>
     </div>
     );
 }
